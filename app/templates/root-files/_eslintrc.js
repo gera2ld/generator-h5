7bd529216c35f720dde1494d08565fed26bb67f7
Gerald <gera2ld@163.com>
2017-11-08 14:08:05 +0800

    fix: update eslint rules

--- app/templates/root-files/_eslintrc.js
+++ app/templates/root-files/_eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: 'babel-eslint',
   extends: 'airbnb-base',
   env: {
     browser: true,
@@ -7,10 +8,13 @@ module.exports = {
     'no-param-reassign': ['error', { props: false }],
     'consistent-return': 'off',
     'no-use-before-define': ['error', 'nofunc'],
-    'no-mixed-operators': ['error', { allowSamePrecedence: true }],
+    'no-mixed-operators': 'off',
     'no-bitwise': ['error', { int32Hint: true }],
     'arrow-parens': ['error', 'as-needed'],
     'prefer-promise-reject-errors': 'off',
     'prefer-destructuring': ['error', { array: false }],
+    'no-console': ['warn', {
+      allow: ['error', 'warn', 'info'],
+    }],
   },
 };
