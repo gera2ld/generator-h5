3f14378564b1f9050902d5b11515d637fb107cf2
Gerald <gera2ld@163.com>
2018-03-21 16:41:15 +0800

    fix: add styles for demos

--- app/templates/_react/demo/index.js
+++ app/templates/_react/demo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 class App extends React.Component {
   state = {
--- app/templates/_vue/demo/app.vue
+++ app/templates/_vue/demo/app.vue
@@ -11,3 +11,9 @@ export default {
   },
 };
 </script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
